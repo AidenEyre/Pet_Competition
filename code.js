18ee04ac203522378587c8e4c037ebc3b0705803
AidenEyre <EyreAiden@gmail.com>
2020-10-03 08:47:31 -0600

    See Description
    -Finished css styling for page.
    -Added 3 images to body and 1 background image to header.
    -Added comments to code where missing.

--- code.js
+++ code.js
@@ -1,7 +1,17 @@
-this.clicked = "petInfo";
+/*
+  JS code to handle the fieldset transitions with html buttons.
+   
+   Author: Aiden Eyre
+   Date:   27 September 2020
+   
+   Filename: code.js
+*/
+
+this.clicked = "petInfo"; // Placeholder of current fieldset.
 
 // I add window.onload so that the DOM can fully load before executing.
 window.onload = function () {
+  // Set up event listener for each fieldset button.
   var petInfoButton = document
     .getElementById("petInfoButton")
     .addEventListener("click", (evt) => {
@@ -26,9 +36,12 @@ window.onload = function () {
       transitions("boarding");
     });
 
+  // Function to change css styles to show a feildset transition.
   var transitions = function (clickedButton) {
     document.getElementById(this.clicked).style.transition = "all 0.6s";
     document.getElementById(this.clicked).style.transform = "scaleY(0)";
+
+    // Wait 600ms to run these functions.
     setTimeout(() => {
       document.getElementById(this.clicked).style.display = "none";
       document.getElementById(this.clicked).style.transform = "scaleY(1)";
